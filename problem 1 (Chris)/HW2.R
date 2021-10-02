29108201f8ae806eeda1f73709d465e0c4668b80
Chris Cherniakov <taequn@gmail.com>
2021-10-02 17:31:12 -0400

    The basic computation and plotting for problem 1

---
+++ problem 1 (Chris)/HW2.R
@@ -0,0 +1,18 @@
+#Alright
+#Explain history.
+#Explain the formulas.
+#Explain CLT.
+
+#b
+#Kolmogorov
+func <- function(x){
+  ((x^3)/64)
+}
+
+integrate(func, 0, 4)
+
+#c
+#Let two sets of parameters be mean = 1 and -2, sd = 1 and 4.
+
+#With this parameters, median would be 1 and -2 respectively due to the nature
+#of normal distribution.
